59abd6a157bcd7a281e5735f4fa851cf3d41b45b
Bowen Sun <ukisami@gmail.com>
2013-12-09 02:49:45 +0800

    [bowen] fix sharing bug

--- app/helpers/share_helper.rb
+++ app/helpers/share_helper.rb
@@ -7,7 +7,7 @@ module ShareHelper
     message += " [b]#{question.choices[1]}"
     message += " [c]#{question.choices[2]}"
     message += " [d]#{@choices[3]}, "
-    if project.id < 12
+    if project.id < 12 or project.share_question_text.nil?
       message += "一起来趣味答题捐#{@project.benefit.short_name}"
     else
       message += project.share_question_text
@@ -19,7 +19,7 @@ module ShareHelper
     message = "【趣味微公益】我今天爱[心]爆棚了！"
     message += "刚刚在“小题大作”网站参与了趣味公益问答，"
     message += "目前累计答对#{correct_count}题。"
-    if project.id < 12
+    if project.id < 12 or project.share_question_text.nil?
       message += "一[din推撞]个人[围观]的力量有限，快来帮帮我吧"
     else
       message += project.share_finish_text
