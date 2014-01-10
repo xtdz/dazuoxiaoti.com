6e309204ba13bbec283cca5b0df7e5ab11aa2422
shiguodong <oreatial@gmail.com>
2014-01-10 11:33:34 +0800

    修改unicorn

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,10 +1,10 @@
-root = "/home/www/apps/alumni/current"
+root = "/home/work/rails/dazuoxiaoti/current"
 working_directory root
 pid "#{root}/tmp/pids/unicorn.pid"
 stderr_path "#{root}/log/unicorn.stderr.log"
 stdout_path "#{root}/log/unicorn.log"
 
-listen "/tmp/unicorn.alumni.sock"
+listen "/tmp/unicorn.dazuoxiaoti.sock"
 worker_processes 4
 timeout 30
 #preload_app true
