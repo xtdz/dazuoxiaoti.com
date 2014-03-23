0e617b2959289d681c2e8f659d18b2fb31fa02fd
Ziyuanliu <ziyuanliu@ziyuanliu-Inspiron-7420.(none)>
2014-03-23 15:34:55 +0800

    修改Mobile分支代码 	modified:   app/controllers/answers_controller.rb 	modified:   app/controllers/application_controller.rb 	modified:   app/controllers/mobile/answers_controller.rb 	deleted:    app/views/answers/_fake.html.haml 	renamed:    app/views/mobile/answers/correct.js.erb -> app/views/answers/correct.js.erb 	deleted:    app/views/answers/fake.js.erb 	renamed:    app/views/mobile/answers/wrong.js.erb -> app/views/answers/wrong.js.erb 	deleted:    app/views/mobile/answers/_fake.html.haml 	modified:   app/views/mobile/answers/_solution.html.haml 	modified:   app/views/mobile/answers/correct.js.erb 	deleted:    app/views/mobile/answers/fake.js.erb 	modified:   app/views/mobile/answers/wrong.js.erb 	deleted:    app/views/mobile/questions/_linked_question.html.haml 	modified:   app/views/mobile/questions/next.js.erb 	deleted:    spec/controllers/mobile/questions_controller_spec.rb 	deleted:    spec/helpers/mobile/questions_helper_spec.rb 	deleted:    spec/views/mobile/questions/questions.html.erb_spec.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -55,7 +55,7 @@ class ApplicationController < ActionController::Base
     end
   end
   def require_mobile_admin
-    admin_ids = [798,50150,53148,54277,54365,54845,54277,57157]
+    admin_ids = [798,50150,53148,54277,54365,54845,57157]
     if !user_signed_in? || !admin_ids.include?(current_user.id)
       redirect_to root_path
     end
