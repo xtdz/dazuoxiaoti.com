c3edf851344d4da79ead35ce88e5d3348057312a
yxc <yxcyanxuecan@gmail.com>
2014-05-17 23:41:46 -0700

    card #78dashboard分享信息修改，因本地无法登陆人人，不知道结果怎样。

--- app/helpers/share_helper.rb
+++ app/helpers/share_helper.rb
@@ -16,9 +16,10 @@ module ShareHelper
   end
 
   def share_record_message correct_count, project
-    message = "【趣味微公益】我今天爱[心]爆棚了！"
-    message += "刚刚在“小题大作”网站参与了趣味公益问答，"
-    message += "目前累计答对#{correct_count}题。"
+    message = "【趣味微公益】最近在“小题大作”网站参与"
+    message += "“#{project.name}”"
+    message += "答题捐赠项目，"
+    message += "累计答对#{correct_count}题。"
     if project.id < 12 or project.share_question_text.nil?
       message += "一[din推撞]个人[围观]的力量有限，快来帮帮我吧"
     else
