ad2444e02297cdc68cef82f2d6521f06301cc897
ProgrammerYuan <ziyuanliu@163.com>
2014-08-29 09:25:59 -0700

    Migrations & Scripts 	新文件：    db/migrate/20140824173936_create_new_table_for_project2.rb 	新文件：    db/migrate/20140829145204_remove_columns_from_projects.rb

---
+++ db/migrate/20140824173936_create_new_table_for_project2.rb
@@ -0,0 +1,84 @@
+class CreateNewTableForProject2 < ActiveRecord::Migration
+  def change
+  	create_table :common_data do |t|
+  	  t.integer :skipped_count , :default => 0
+  	  t.integer :correct_count , :default => 0
+  	  t.integer :incorrect_count , :default => 0
+  	  t.integer :participation_count , :default => 0
+  	  t.integer :limit , :default => 0
+  	  t.integer :rate , :default => 1
+  	  t.integer :coordinator_id
+  	  t.integer :benefit_id
+  	  t.integer :sponsor_id
+  	  t.datetime :start_time
+  	  t.datetime :end_time
+  	  t.string :tagline
+  	  t.text :description
+  	  t.string :name
+  	  t.integer :feedback_image_count , :default => 0
+  	  t.boolean :hidden , :default => false
+  	  t.integer :sharable_id
+  	  t.string :sharable_type
+  	  t.string :information
+  	  t.string :share_question_text
+  	  t.string :share_finish_text
+  	  t.integer :unit_rate , :default => 1
+  	  t.string :banner_url
+      t.has_attached_file :upload_image_main
+      t.has_attached_file :upload_image_about
+      t.has_attached_file :upload_image_small
+      t.has_attached_file :upload_image_banner
+      t.has_attached_file :upload_image_share_question1
+      t.has_attached_file :upload_image_share_question2
+      t.has_attached_file :upload_image_share_finish1
+      t.has_attached_file :upload_image_share_finish2
+      t.timestamps
+  	end
+  	create_table :project2s do |t|
+  		t.string :coordinator_label
+  		t.string :equation_string
+  		t.string :tagline2
+  		t.string :label_content
+  	end
+  	CommonData.reset_column_information
+  	Project2.reset_column_information
+  	Project.all.each do |p|
+  		data = CommonData.new
+  		data.skipped_count = p.skipped_count
+  		data.correct_count = p.correct_count
+  		data.incorrect_count = p.incorrect_count
+  		data.participation_count = p.participation_count
+  		data.limit = p.limit
+  		data.rate = p.rate
+  		data.coordinator_id = p.coordinator_id
+  		data.benefit_id = p.benefit_id
+  		data.sponsor_id = p.sponsor_id
+  		data.start_time = p.start_time
+  		data.end_time = p.end_time
+  		data.tagline = p.tagline
+  		data.description = p.description
+  		data.name = p.name
+  		data.feedback_image_count = p.feedback_image_count
+  		data.hidden = p.hidden
+  		data.sharable_id = p.id
+  		if p.project_kind == 1
+  			data.sharable_type = 'Project'
+  		else
+  			data.sharable_type = 'Project2'
+  		end
+  		data.unit_rate = p.unit_rate
+  		data.banner_url = p.banner_url
+  		data.upload_image_main = p.upload_image_main if p.upload_image_main.exists?
+      data.upload_image_about = p.upload_image_about if p.upload_image_about.exists?
+      data.upload_image_small = p.upload_image_small if p.upload_image_small.exists?
+      data.upload_image_banner = p.upload_image_banner if p.upload_image_banner.exists?
+      data.upload_image_share_finish1 = p.upload_image_share_finish1 if p.upload_image_share_finish1.exists?
+      data.upload_image_share_finish2 = p.upload_image_share_finish2 if p.upload_image_share_finish2.exists?
+      data.upload_image_share_question1 = p.upload_image_share_question1 if p.upload_image_share_question1.exists?
+      data.upload_image_share_question2 = p.upload_image_share_question2 if p.upload_image_share_question2.exists?
+  		data.sharable = p
+  		data.save
+  		p.save
+  	end
+  end
+end
---
+++ db/migrate/20140829145204_remove_columns_from_projects.rb
@@ -0,0 +1,33 @@
+class RemoveColumnsFromProjects < ActiveRecord::Migration
+  def change
+  	remove_column :projects, :skipped_count
+  	remove_column :projects, :correct_count
+  	remove_column :projects, :incorrect_count
+  	remove_column :projects, :participation_count
+  	remove_column :projects, :limit
+  	remove_column :projects, :rate
+  	remove_column :projects, :coordinator_id
+  	remove_column :projects, :benefit_id
+  	remove_column :projects, :sponsor_id
+  	remove_column :projects, :start_time
+  	remove_column :projects, :end_time
+  	remove_column :projects, :tagline
+  	remove_column :projects, :description
+  	remove_column :projects, :name
+  	remove_column :projects, :feedback_image_count
+  	remove_column :projects, :hidden
+  	remove_column :projects, :information
+  	remove_column :projects, :share_question_text
+  	remove_column :projects, :share_finish_text
+  	remove_column :projects, :unit_rate
+  	remove_column :projects, :banner_url
+    remove_attachment :projects, :upload_image_main
+    remove_attachment :projects, :upload_image_about
+    remove_attachment :projects, :upload_image_small
+    remove_attachment :projects, :upload_image_banner
+    remove_attachment :projects, :upload_image_share_question1
+    remove_attachment :projects, :upload_image_share_question2
+    remove_attachment :projects, :upload_image_share_finish1
+    remove_attachment :projects, :upload_image_share_finish2  
+  end
+end
