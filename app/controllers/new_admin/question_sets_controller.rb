81010b7da3c0432d8ee817117d9da457a51407c3
ProgrammerYuan <ziyuanliu@163.com>
2014-11-11 12:40:40 +0800

    为题集后台添加题集导出功能（将题目以Excel格式导出） 	modified:   app/controllers/new_admin/question_sets_controller.rb 	modified:   app/views/new_admin/question_sets/index.html.erb 	modified:   config/routes.rb

--- app/controllers/new_admin/question_sets_controller.rb
+++ app/controllers/new_admin/question_sets_controller.rb
@@ -34,6 +34,36 @@ class NewAdmin::QuestionSetsController < NewAdmin::ApplicationController
     @question_set = QuestionSet.find(params[:id])
   end
 
+  def export
+    @question_set = QuestionSet.find(params[:id])
+    respond_to do |format| 
+      format.xls { 
+        send_data(to_xls(@question_set), :type=>:xls, :filename => "question_set_#{@question_set.id}.xls")
+      }
+    end
+  end
+  def to_xls question_set, name = "test"
+    book = Spreadsheet::Workbook.new
+    data = book.create_worksheet :name => name
+    data.row(0).concat %w{序号 题干 选项1 选项2 选项3 选项4 正确选项 解释}
+    header_format = Spreadsheet::Format.new :color => :green, :weight => :bold
+    data.row(0).default_format = header_format
+    all_questions = question_set.questions
+    all_questions.each_with_index do |q,index|
+      data.row(index + 1)[0] = index + 1
+      data.row(index + 1)[1] = q.title
+      data.row(index + 1)[2] = q.c1
+      data.row(index + 1)[3] = q.c2
+      data.row(index + 1)[4] = q.c3
+      data.row(index + 1)[5] = q.c4
+      data.row(index + 1)[6] = q.correct_index + 1
+      data.row(index + 1)[7] = q.explanation
+    end
+    blob = StringIO.new('')
+    book.write(blob)
+    blob.string
+  end
+
   def update
     @question_set = QuestionSet.find(params[:id])
 
